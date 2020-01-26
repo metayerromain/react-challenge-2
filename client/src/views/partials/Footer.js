6bbbf0614dca8580a38f0102def6fe94c4a55ead
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-01-26 16:34:54 +0100

    Config / add example

---
+++ client/src/views/partials/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <Footer className={'footer-partial'}>
+            <h1> This is a footer partial </h1>
+        </Footer>
+    )
+}
+
+export default Footer;
