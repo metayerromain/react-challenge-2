c6a6ad9e09d08b6381871b98f0d0bbd4caba3bc3
Romain Metayer <33374185+metayerromain@users.noreply.github.com>
2020-01-29 10:39:07 +0100

    Update Home.js

--- client-web/src/views/pages/Home.js
+++ client-web/src/views/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return(
         <div className={'home-page'}>
-            <p> This is a Home page </p>
+            <p> This is a Home page. </p>
         </div>
     )
 }
